6a7a1c3555de791d586f466c0f693e58878964ab
romain.ducret1 <romain.ducret1@IG13-DUCRETR.intra.eiaj.ch>
2015-12-17 08:56:45 +0100

    Ajout des classes business.

---
+++ src/java/ch/hearc/ig/odi/movieapp/business/Movie.java
@@ -0,0 +1,88 @@
+/*
+ *  Translat SA vous présente son projet de gestion de Projet Agile.
+ *  Copyleft 2015 Translait.
+ *   /\_/\
+ * =( °w° )=
+ *   )   (  //
+ *  (__ __)//
+ */
+package ch.hearc.ig.odi.movieapp.business;
+
+/**
+ *
+ * @author Romain Ducret <romain.ducret1@he-arc.ch>
+ */
+public class Movie {
+    private Long id;
+    private String name;
+    private String producer;
+
+    /**
+     * Constructeur de Movie
+     * 
+     * @param id
+     * @param name
+     * @param producer 
+     */
+    public Movie(final Long id,final String name,final String producer) {
+        this.id = id;
+        this.name = name;
+        this.producer = producer;
+    }
+
+    /**
+     * Retourne l'id du film
+     * 
+     * @return 
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Met à jour l'id du film
+     * 
+     * @param id 
+     */
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    /**
+     * Retourne le nom du film
+     * 
+     * @return 
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Met à jour le nom du film
+     * 
+     * @param name 
+     */
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    /**
+     * Retourne le producteur du film
+     * 
+     * @return 
+     */
+    public String getProducer() {
+        return producer;
+    }
+
+    /**
+     * Met à jour le producteur du film
+     * 
+     * @param producer 
+     */
+    public void setProducer(final String producer) {
+        this.producer = producer;
+    }
+    
+    
+}
---
+++ src/java/ch/hearc/ig/odi/movieapp/business/Person.java
@@ -0,0 +1,90 @@
+/*
+ *  Translat SA vous présente son projet de gestion de Projet Agile.
+ *  Copyleft 2015 Translait.
+ *   /\_/\
+ * =( °w° )=
+ *   )   (  //
+ *  (__ __)//
+ */
+package ch.hearc.ig.odi.movieapp.business;
+
+/**
+ *
+ * @author Romain Ducret <romain.ducret1@he-arc.ch>
+ */
+public class Person {
+    private Long id;
+    private String firstname;
+    private String lastname;
+    
+    /**
+     * Constructeur person
+     * 
+     * @param id
+     *        L'identifiant unique de la personne.
+     * @param firstname
+     *        Le prénom de la personne.
+     * @param lastname 
+     *        Le nom de la personne.
+     */
+    public Person(final Long id,final String firstname,final String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    
+    /**
+     * Retourne l'id de la personne
+     * 
+     * @return 
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Met à jour l'id de la personne
+     * 
+     * @param id 
+     */
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    /**
+     * Retourne la prénom de la personne
+     * 
+     * @return 
+     */
+    public String getFirstname() {
+        return firstname;
+    }
+
+    /**
+     * Met à jour le prénom de la personne
+     * 
+     * @param firstname 
+     */
+    public void setFirstname(final String firstname) {
+        this.firstname = firstname;
+    }
+
+    /**
+     * Retourne le nom de la personne
+     * 
+     * @return 
+     */
+    public String getLastname() {
+        return lastname;
+    }
+
+    /**
+     * Met à jour le nom de la personne.
+     * 
+     * @param lastname 
+     */
+    public void setLastname(final String lastname) {
+        this.lastname = lastname;
+    }
+    
+}
